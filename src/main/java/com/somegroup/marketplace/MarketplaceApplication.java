059b19b8e20f8395492a7cc9af320d4b37f475bb
r3v1zor <ther3v1zor@gmail.com>
2021-12-20 16:14:54 +0400

    add logging (2)

--- src/main/java/com/somegroup/marketplace/MarketplaceApplication.java
+++ src/main/java/com/somegroup/marketplace/MarketplaceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Collectors;
@@ -14,8 +15,7 @@ import java.util.stream.Collectors;
 public class MarketplaceApplication {
 
 
-	@SneakyThrows
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println(Files.list(Path.of("/usr/local/app/db/migration/")).collect(Collectors.toList()));
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
