180e2354432afebe1dfac3a62fc089efb0fbef31
r3v1zor <ther3v1zor@gmail.com>
2021-12-20 13:48:59 +0400

    add logging

--- src/main/java/com/somegroup/marketplace/MarketplaceApplication.java
+++ src/main/java/com/somegroup/marketplace/MarketplaceApplication.java
@@ -1,14 +1,22 @@
 package com.somegroup.marketplace;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class MarketplaceApplication {
 
+
+	@SneakyThrows
 	public static void main(String[] args) {
+		System.out.println(Files.list(Path.of("/usr/local/app/db/migration/")).collect(Collectors.toList()));
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
 
