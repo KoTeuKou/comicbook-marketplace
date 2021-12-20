54a0237ae22ebecd9e0683e43a449ad4616c1c29
r3v1zor <ther3v1zor@gmail.com>
2021-12-20 16:24:41 +0400

    fix flyway location parameter

--- src/main/java/com/somegroup/marketplace/MarketplaceApplication.java
+++ src/main/java/com/somegroup/marketplace/MarketplaceApplication.java
@@ -1,22 +1,15 @@
 package com.somegroup.marketplace;
 
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class MarketplaceApplication {
 
 
-	public static void main(String[] args) throws IOException {
-		System.out.println(Files.list(Path.of("/usr/local/app/db/migration/")).collect(Collectors.toList()));
+	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
 
